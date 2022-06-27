97586178951587bcf5d798020af8d591ed360b61
Kim-Young-Joon <yj3723@naver.com>
2022-06-27 15:17:52 +0900

    ejs 서버

--- 서버_ejs.js
+++ 서버_ejs.js
@@ -15,8 +15,15 @@ let server = http.createServer((req, res) => {
 
         res.end(ejs.render(data, {
             title: "EJS 엔진을 배워봅시다",
-            contents: "JSON 형태로 데이터를 보내주면 html과 결합하여 새로운 문서를 만든다."
-        }));
+            contents: "JSON 형태로 데이터를 보내주면 html과 결합하여 새로운 문서를 만든다.",
+            fruits: ["사과", "배", "거봉", "망고", "참외", "수박"],
+            product: [
+                {name:"노트북", price:2000000},
+                {name:"에어컨", price:3500000},
+                {name:"냉장고", price:4000000},
+                {name:"갤럭시", price:1000000}
+            ]
+        })); // JSON 형태로 데이터를 전송해서, html 문서에서 받아서 사용할 수 있다.
     })
 
 });
