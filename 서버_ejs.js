49039c2db6a74fea104602e873e547a3599f998a
Kim-Young-Joon <yj3723@naver.com>
2022-06-27 15:04:23 +0900

    ejs 서버

--- 서버_ejs.js
+++ 서버_ejs.js
@@ -10,8 +10,13 @@ let server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader("Content-type", "text/html");
         // html 파일을 ejs 엔진을 이용해 렌더링 한다.
+        // ejs의 render의 첫번째 인자는 파일의 내용
+        // 두번째 인자는 JSON 형태로 데이터를 전송한다.
 
-        res.end(ejs.render(data));
+        res.end(ejs.render(data, {
+            title: "EJS 엔진을 배워봅시다",
+            contents: "JSON 형태로 데이터를 보내주면 html과 결합하여 새로운 문서를 만든다."
+        }));
     })
 
 });
